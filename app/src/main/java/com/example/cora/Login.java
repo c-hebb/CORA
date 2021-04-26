119d231a7c6ba111783385acc41f38254c9d3448
c-hebb <work@hebb.dev>
2021-04-25 23:34:18 -0500

    Merged the two to work well and run.

--- app/src/main/java/com/example/cora/Login.java
+++ app/src/main/java/com/example/cora/Login.java
@@ -69,7 +69,7 @@ public class Login extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         }else {
                             Toast.makeText(Login.this, "Login Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
